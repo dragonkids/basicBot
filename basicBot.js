d77668f48977bc4607a467a10fc41b453a9e68fe
dragonkids <toshibadt01@outlook.com>
2015-05-10 17:47:04 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1586,18 +1586,6 @@
                     }
                 }
             },
-             hahaCommand: {
-                   command: 'haha',
-                   rank: 'user',
-                   type: 'exact',
-                   functionality: function (chat, cmd) {
-                       if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        API.sendChat(basicBot.chat.haha);
-                    }
-                }
-            },
 
             ballCommand: {
                 command: ['8ball', 'ask'],
